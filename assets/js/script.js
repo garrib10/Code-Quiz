1b522be7eeec9c217f38b0992d1c1b0da881a080
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-08-26 00:02:04 -0400

    fixed timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,11 @@ var startButton = document.getElementById("start");
 var startScreen = document.getElementById("start-screen");
 var quizScreen = document.getElementById("quiz-screen");
 var timerEl = document.getElementById("timer")
+var choiceA =document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var choiceD = document.getElementById("D");
+
 
 var timer = 60;
 var timerInterval = setInterval(clocktick, 1000)
@@ -15,4 +20,10 @@ quizScreen.removeAttribute("class", "hide");
 function clocktick (){
  timer--;
 timerEl.textContent = timer;
+if (timer ===0){
+    clearInterval(timerInterval);
+    return ;
+}  
+ 
 }
+
