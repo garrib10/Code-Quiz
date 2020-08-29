8183834768750dfb556ad9de9c695e505fdbc625
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-08-29 19:04:06 -0400

    quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,32 +4,58 @@ var quizScreen = document.getElementById("quiz-screen");
 var questionEl = document.getElementById("questions");
 var timerEl = document.getElementById("timer");
 var scoreEl = document.getElementById("score");
+var highscoreName=document.getElementById("high-score-intials");
+var highscoreDisplay = document.getElementById("high-score");
 var buttonA =document.getElementById("A");
 var buttonB = document.getElementById("B");
 var buttonC = document.getElementById("C");
 var buttonD= document.getElementById("D");
 
+// Quiz Questions 
+var quizQuestions = [{
+    question: "What was the Beatle's Debut Album?",
+    choiceA: "Yellow Submarine",
+    choiceB: "Please Please Me",
+    choiceC: "A Hard Day's Night",
+    choiceD: "Abbey Road",
+    correctAnswer: "B"},
+  {
+    question: "What was the Beach Boy's Debut Album?",
+    choiceA: "Surfin' Safari",
+    choiceB: "Pet Sounds",
+    choiceC: "Surfer Girl",
+    choiceD: "Smiley Smile",
+    correctAnswer: "A"},
+   {
+    question: "What was Ozzy Osbourne debut Solo Album?",
+    choiceA: "Bark at the Moon",
+    choiceB: "Diary of A Madman",
+    choiceC: "Blizzard of Ozz",
+    choiceD: "No Rest for the Wicked",
+    correctAnswer: "C"},
+    {
+    question: "What was Led Zepplelin debut album?",
+    choiceA: "Physical Graffiti",
+    choiceB: "Houses of the Holy",
+    choiceC: "Presence",
+    choiceD: "Led Zeppelin;",
+    correctAnswer: "D"},
+
+ ];
 
 // Other global variables
+var finalQuestionIndex=quizQuestions.length;
 var currentQuestionIndex =0;
 var timer = 60;
 var timerInterval = setInterval(clocktick, 1000);
 var scoreEl = 0;
 
-// Quiz Questions 
 
-
-
-
-
-//For loop for timer 
-  
-
-
-startButton.addEventListener("click", function(){
+  startButton.addEventListener("click", function(){
     //make the start screen disapear
 startScreen.setAttribute("class", "hide");
 quizScreen.removeAttribute("class", "hide");
+
 });
 
 function clocktick (){
@@ -37,7 +63,7 @@ function clocktick (){
 timerEl.textContent = timer;
 if (timer ===0){
     clearInterval(timerInterval);
-    return ;
+    showScore () ;
 } 
 
 
